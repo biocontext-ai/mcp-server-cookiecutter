6cb0f096f8e97b4e6fd91c8dc9e979eec9360709
Malte <harryhaller001@gmail.com>
2025-09-15 19:40:31 +0200

    fix linting errors, adding pytest asyncio as plugin, adding working mcp server test

--- {{cookiecutter.project_name}}/tests/test_app.py
+++ {{cookiecutter.project_name}}/tests/test_app.py
@@ -1,13 +1,17 @@
-from fastmcp import FastMCP, Client
+import pytest
+from fastmcp import Client
 
-from {{cookiecutter.package_name}} import __version__, mcp
+import {{cookiecutter.package_name}}
 
 
 def test_package_has_version():
     """Testing package version exist."""
     assert {{cookiecutter.package_name}}.__version__ is not None
 
+
+@pytest.mark.asyncio
 async def test_mcp_server():
     """Testing MCP server."""
-    async with Client(mcp) as client:
-        await client.call_tool("greet", {"name": "test"})
+    async with Client({{cookiecutter.package_name}}.mcp) as client:
+        result = await client.call_tool("greet", {"name": "test"})
+        assert result.data == "Hello, test!"
